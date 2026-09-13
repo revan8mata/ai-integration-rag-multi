3579c46c82b837157155944d086fb7489a852703
revan8mata <tavenhall1000@gmail.com>
2026-09-13 05:50:32 -0800

    minimal frontend

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import config
 from config import settings
 from fastapi.middleware.cors import CORSMiddleware
 from database import get_db
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 origins = ["*"]
@@ -33,6 +34,10 @@ app.include_router(users.ROUTER)
 app.include_router(conversations.ROUTER)
 app.include_router(messages.ROUTER)
 app.include_router(documents.ROUTER)
+
+
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 client = genai.Client(api_key=settings.api_key)
 
